fbf5bae654cf00ef00f05b9bf6db6d51c8a340a0
sophiaibanez <57376444+sophiaibanez@users.noreply.github.com>
2019-12-07 21:44:52 +0800

    Add files via upload

---
+++ [MATLAB] Machine Problem 2.m
@@ -0,0 +1,18 @@
+x1 = input('Enter value for X1:  ');
+y1 = input('For Y1:  ');
+x2 = input('For X2:  ');
+y2 = input('For Y2:  ');
+x3 = input('For X3:  ');
+y3 = input('And for Y3:  ');
+fprintf('Points: (%.f,%.f), (%.f,%.f) and (%.f,%.f). \n', x1,y1,x2,y2,x3,y3)
+
+A = x1*(y2-y3) - y1*(x2-x3) + x2*y3 - x3*y2;
+D = (x1^2 + y1^2)*(y3-y2) + (x2^2 + y2^2)*(y1-y3) + (x3^2 + y3^2)*(y2-y1);
+E = (x1^2 + y1^2)*(x2-x3) + (x2^2 + y2^2)*(x3-x1) + (x3^2 + y3^2)*(x1-x2);
+F = (x1^2 + y1^2)*(x3*y2 - x2*y3) + (x2^2 + y2^2)*(x1*y3 - x3*y1) + (x3^2 + y3^2)*(x2*y1 - x1*y2);
+
+h = -(D/(2*A));
+k = -(E/(2*A));
+r = sqrt((D^2 + E^2 -4*A*F)/(4*A^2));
+fprintf('Given 3 points, the circle formed has its at center (%.f,%.f) with radius of %.f units. \n', h,k,r)
+fprintf('The vector would be: V[%.f,%.f,%.f].',D,E,F)
---
+++ [MATLAB] Machine Problem 4.m
@@ -0,0 +1,36 @@
+y0 = input('Enter initial height:  ');
+v0 = input('Enter velocity:  ');
+angle = input('Enter angle (with respect to x-axis): ');
+ax = input('Enter initial acceleration in x:  ');
+ay = input('Enter initial acceleration in y:  ');
+
+if ay == 0
+    error('Error. There is no vertical acceleration.')
+
+else
+    x0 = 0;
+    vx_initial = v0 * cosd(angle);
+    vy_initial = v0 * sind(angle);
+    x = x0 + vx_initial .* t + (1/2) * ax .* t .^2;
+    y = y0 + vy_initial .* t + (1/2) * ay .* t .^2;
+    tf = roots([(1/2)*ay, vy_initial, y0]);
+    tf = max(tf);
+    t = [0:0.1:tf];
+    
+    plot(x, y, 'LineWidth', 2);
+    title ('Projectile Trajectory');
+    xlabel('Horizontal distance (in meters)');
+    ylabel('Height (in meters)');
+    grid on;
+    axis tight;
+    hold on
+    
+    ax_ideal = 0;
+    ay_ideal = -9.8;
+    x_ideal = x0 + vx_initial .* t;
+    y_ideal = y0 + vy_initial .* t + (1/2) * ay_ideal .* t .^2;
+    
+    plot(x_ideal, y_ideal, '-r', 'LineWidth', 2)
+    legend('Non-ideal Trajectory','Ideal Trajectory')
+ 
+end
